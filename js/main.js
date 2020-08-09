859346393babdf4ca9111e1e5375c1e16d7abdad
Nandakishore-Menon <nandakishore.menon@iiitb.org>
2020-08-09 19:27:14 +0530

    a few more bugs to fix

--- js/main.js
+++ js/main.js
@@ -33,6 +33,21 @@ const slide=()=>{
 			});
 		});
 
+	$('.tabs a').click(function(){
+  
+     $('.panel').hide();
+     $('.tabs a.active').removeClass('active');
+     $(this).addClass('active');
+     
+     var panel = $(this).attr('href');
+     $(panel).fadeIn(1000);
+     
+     return false;  // prevents link action
+    
+  });  // end click 
+
+     $('.tabs li:first a').click();
+     
 
 	
 
@@ -66,26 +81,10 @@ $(document).ready(function(){
 
 
 
-$(document).ready(function() {
     
     //alert('here');
 	
-  $('.tabs a').click(function(){
-  
-     $('.panel').hide();
-     $('.tabs a.active').removeClass('active');
-     $(this).addClass('active');
-     
-     var panel = $(this).attr('href');
-     $(panel).fadeIn(1000);
-     
-     return false;  // prevents link action
-    
-  });  // end click 
 
-     $('.tabs li:first a').click();
-     
-}); // end ready
 
 
 
