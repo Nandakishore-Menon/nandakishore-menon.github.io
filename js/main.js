7a850e599b5cd26d1029d63ba1c1fc769a415013
Nandakishore-Menon <nandakishore.menon@iiitb.org>
2020-08-09 15:45:42 +0530

    basic functionality added

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
 /*
 navbar
 */
+var func=()=>{
+		const nav=document.querySelector('.navlinks');
+		const navlinks=document.querySelectorAll('.navlinks li');
+		nav.classList.toggle('nav-active');
+		navlinks.forEach((link,index)=>{
+			if(link.style.animation){
+				link.style.animation='';
+
+			}
+			else{
+			
+				link.style.animation=`navlinksFade 0.5s ease forwards ${index/7+0.5}s`;
+
+			}
+
+		});
+	};
 const slide=()=>{
 	const burger=document.querySelector('.burger');
 	const nav=document.querySelector('.navlinks');
 	const navlinks=document.querySelectorAll('.navlinks li');
 
 
-	burger.addEventListener('click',()=>{
-		nav.classList.toggle('nav-active');
-		navlinks.forEach((link,index)=>{
-			if(link.style.animation){
-				link.style.animation='';
-			}
-			else{
-			
-				link.style.animation=`navlinksFade 0.5s ease forwards ${index/7+0.5}s`;
-			
-			}
+	burger.addEventListener('click',func);
 
+	navlinks.forEach((link,index)=>{
+			link.addEventListener('click',()=>{
+				// $('.navlinks').toggleClass('nav-active');
+				func();
+			});
 		});
 
 
-	});
+	
 
 };
 
@@ -31,3 +43,26 @@ slide();
 $('.carousel').on('slid.bs.carousel', function () {
     $('.projinfo').html($('.active > .carousel-caption').html());
 });
+
+
+
+$(document).ready(function(){
+
+  $(".navitems").on('click', function(event) {
+    if (this.hash !== "") {
+
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+
+
+
+
